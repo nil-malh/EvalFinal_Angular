b7339407930834eda4137445771b93162cd779d5
Nil MALHOMME <malhomme.nil+github@gmail.com>
2023-03-03 12:32:26 +0100

    That was painful, everything should work !

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -11,40 +11,40 @@ import {ICountyResponse} from "../interfaces/ICountyResponse";
 })
 export class CitiesComponent {
 
-  constructor(private httpClient : HttpClient, private activatedRoute: ActivatedRoute) {
+  constructor(private httpClient: HttpClient, private activatedRoute: ActivatedRoute) {
+  }
+  public setDepartementName(code: string): void {
+    let apiURLCounty = "https://geo.api.gouv.fr/departements/%";
+    let name: string;
+    this.httpClient.get<ICountyResponse>(apiURLCounty.replace("%", code));
+    this.httpClient.get<ICountyResponse>(apiURLCounty.replace("%", code)).subscribe(value => {
+       this.countyName = value.nom;
+      }
+    );
   }
   cities: ICityResponse[] = [];
-
+  countyName: string = ";"
   ngOnInit() {
     let apiURLCities = "https://geo.api.gouv.fr/departements/%/communes";
+
+
     console.log(this.activatedRoute.snapshot.params)
     if (this.activatedRoute.snapshot.params['code']) {
+
       this.activatedRoute.params.subscribe((params) => {
-        console.log(apiURLCities.replace("%",params['zip']))
-        this.httpClient.get<ICityResponse[]>(apiURLCities.replace("%",params['zip']))
+      this.setDepartementName(params['zip']);
+        console.log(apiURLCities.replace("%", params['zip']))
+        this.httpClient.get<ICityResponse[]>(apiURLCities.replace("%", params['zip']))
           .subscribe(
             (response) => {
               this.cities = response;
             }
-
           )
         console.log(this.activatedRoute.snapshot.params);
       });
-    };
+    }
+    ;
   }
 
-  public getDepartementName(code: string): string
-  {
-    let apiURLCounty = "https://geo.api.gouv.fr/departements/%";
-    let county : any;
-    this.httpClient.get<ICountyResponse>(apiURLCounty.replace("%",code))
-          .subscribe(
-            (response) => {
-              county = response;
-            }
-          )
-
-    return "name";
-  }
 
 }
